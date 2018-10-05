e50efa8f4dfc6deed125138789f9be998f01436d
cagabi <carlos@carbon.coop>
2018-10-05 16:58:07 +0100

    travis: deployment moved to script

---
+++ travis-CI/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SSH setup for deployment
+cd /var/www/html/emoncms/Modules/assessment/
+openssl aes-256-cbc -K $encrypted_d561feca5f1b_key -iv $encrypted_d561feca5f1b_iv -in deploy_key.enc -out ./deploy_key -d
+chmod 600 ./deploy_key
+echo -e "Host $PRODUCTION_SERVER\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh -i ./deploy_key $PRODUCTION_SERVER@$PRODUCTION_SERVER
+
+# Run deployment script
+sh deployer.sh 
