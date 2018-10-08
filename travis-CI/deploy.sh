891e6e92f7f34377a26d85e958713c61ccb426c6
cagabi <carlos@carbon.coop>
2018-10-08 16:06:45 +0100

    fir ssh command

--- travis-CI/deploy.sh
+++ travis-CI/deploy.sh
@@ -5,7 +5,7 @@ cd /var/www/html/emoncms/Modules/assessment/
 openssl aes-256-cbc -K $encrypted_d561feca5f1b_key -iv $encrypted_d561feca5f1b_iv -in deploy_key.enc -out ./deploy_key -d
 chmod 600 ./deploy_key
 echo -e "Host $PRODUCTION_SERVER\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh -i ./deploy_key $PRODUCTION_SERVER@$PRODUCTION_SERVER
+ssh -i ./deploy_key $PRODUCTION_SERVER_USER@$PRODUCTION_SERVER
 
 # Run deployment script
-sh deployer.sh 
+#sh deployer.sh 
