e1696e151af719f058683ce6cd922aa49bf1d262
cagabi <carlos@carbon.coop>
2018-10-08 17:18:32 +0100

    run deployer script from ssh command

--- travis-CI/deploy.sh
+++ travis-CI/deploy.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-
-# SSH setup for deployment
+#!/bin/bash
 cd /var/www/html/emoncms/Modules/assessment/
-openssl aes-256-cbc -K $encrypted_d561feca5f1b_key -iv $encrypted_d561feca5f1b_iv -in deploy_key.enc -out ./deploy_key -d
+openssl aes-256-cbc -K $encrypted_d561feca5f1b_key -iv $encrypted_d561feca5f1b_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 ./deploy_key
-echo -e "Host $PRODUCTION_SERVER\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh -i ./deploy_key $PRODUCTION_SERVER_USER@$PRODUCTION_SERVER
-
-# Run deployment script
-#sh deployer.sh 
+echo "Host $PRODUCTION_SERVER\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh -i ./deploy_key $PRODUCTION_SERVER_USER@$PRODUCTION_SERVER "~/deployer.sh"
